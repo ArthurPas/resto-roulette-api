9ed3606829d707e05ed3509d932cd117713ae61b
arthur <arthur.pascal33@gmail.com>
2025-09-04 13:06:01 +0200

    setup spring app

---
+++ resto-roulette-api/src/main/java/com/roulette/resto/RestoRouletteApiApplication.java
@@ -0,0 +1,13 @@
+package com.roulette.resto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoRouletteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoRouletteApiApplication.class, args);
+	}
+
+}
---
+++ resto-roulette-api/src/test/java/com/roulette/resto/RestoRouletteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roulette.resto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoRouletteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

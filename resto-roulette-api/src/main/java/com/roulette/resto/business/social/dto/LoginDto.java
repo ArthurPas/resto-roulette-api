18930c7d8dc1d2b5cbc49b9da3852d839411357c
arthur <arthur.pascal33@gmail.com>
2025-09-23 09:08:51 +0200

    add swagger docs and examples

--- resto-roulette-api/src/main/java/com/roulette/resto/business/social/dto/LoginDto.java
+++ resto-roulette-api/src/main/java/com/roulette/resto/business/social/dto/LoginDto.java
@@ -1,14 +1,19 @@
 package com.roulette.resto.business.social.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginDto {
+	@Schema(example = "fan2resto", requiredMode = REQUIRED)
 	String login;
+	@Schema(example = "azerty123", requiredMode = REQUIRED)
 	String password;
 
 }

88f443fb357cc6d27ec2b508fd110c334add6d7a
arthur <arthur.pascal33@gmail.com>
2025-10-07 10:40:38 +0200

    kpi registration trend

---
+++ resto-roulette-api/src/main/java/com/roulette/resto/business/administration/dto/out/GlobalUserStatDto.java
@@ -0,0 +1,14 @@
+package com.roulette.resto.business.administration.dto.out;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GlobalUserStatDto {
+	private long totalRegistered;
+	private float variation;
+	private VARIATION variationType;
+}

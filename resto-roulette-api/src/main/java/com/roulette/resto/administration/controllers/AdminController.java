b27db574cabedced3209f14af5af784456b15f64
arthur <arthur.pascal33@gmail.com>
2025-11-01 13:35:30 +0100

    update-role endpoint

--- resto-roulette-api/src/main/java/com/roulette/resto/administration/controllers/AdminController.java
+++ resto-roulette-api/src/main/java/com/roulette/resto/administration/controllers/AdminController.java
@@ -1,14 +1,25 @@
 package com.roulette.resto.administration.controllers;
 
 import com.roulette.resto.administration.services.AdminService;
+import com.roulette.resto.common.exception.APIError;
+import com.roulette.resto.common.exception.ErrorResponse;
+import com.roulette.resto.social.dto.in.UpdateAccountInfo;
+import com.roulette.resto.social.dto.out.UserInfoDto;
 import com.roulette.resto.social.services.AccountService;
 import com.roulette.resto.social.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import javax.security.auth.login.AccountNotFoundException;
 
 @Slf4j
 @RestController
@@ -28,14 +39,35 @@ public class AdminController {
 	@GetMapping("/users")
 	public ResponseEntity<?> getTotalUsersRegistrations(Authentication authentication, @RequestParam(required = false
 			,defaultValue = "0")int page) {
-		final ResponseEntity<?> UNAUTHORIZED = adminService.rightCheckIsAdmin(authentication);
-		if(UNAUTHORIZED != null) return UNAUTHORIZED;
 		try {
+			adminService.rightCheckIsAdmin(authentication);
 			return new ResponseEntity<>(userService.getAccounts(page), HttpStatus.OK);
-		}catch (Exception e) {
-			log.error(e.getMessage());
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (APIError e) {
+			return new ResponseEntity<>(new ErrorResponse(e.getMessage()), e.getStatus());
 		}
 	}
+
+	@PatchMapping("/update-role")
+	@Operation(summary = "Change account role", description = "Change the role of the account" +
+			" The role values available are : ROLE_USER, ROLE_RESTAURANT_OWNER, ROLE_MODERATOR, ROLE_ADMIN")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200",
+					description = "Total and trend data",
+					content = @Content(mediaType = "application/json",
+							schema = @Schema(implementation = UserInfoDto.class)))})
+	public ResponseEntity<?> modifyAccountRole (Authentication authentication,
+												 @RequestBody UpdateAccountInfo updateAccountInfo) {
+		try {
+			adminService.rightCheckIsAdmin(authentication);
+			int accountId = userService.updateAccountInfo(updateAccountInfo);
+			UserInfoDto userInfoDto = userService.getUserInfoById(accountId);
+			return new ResponseEntity<>(userInfoDto, HttpStatus.OK);
+		} catch (APIError e) {
+			return new ResponseEntity<>(new ErrorResponse(e.getMessage()), e.getStatus());
+		} catch (AccountNotFoundException e) {
+			return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.NOT_FOUND);
+		}
+	}
+
 
 }

b31e5d56359fd272947edcc31035028af153f407
arthur <arthur.pascal33@gmail.com>
2025-12-05 15:39:17 +0100

    Global exception handler

--- src/main/java/com/roulette/resto/configuration/GlobalExceptionHandler.java
+++ src/main/java/com/roulette/resto/configuration/GlobalExceptionHandler.java
@@ -1,16 +1,24 @@
 package com.roulette.resto.configuration;
 
+import com.roulette.resto.exception.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import java.util.Arrays;
+
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<?> handleServerError(Exception ex) {
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-				.body("Internal Server Error");
+	public ResponseEntity<?> handleGeneralException(Exception ex, HttpServletRequest request) {
+		log.error("Exception caught in GlobalExceptionHandler", ex);
+		log.error(ex.getMessage());
+		log.error(request.getContextPath());
+		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }

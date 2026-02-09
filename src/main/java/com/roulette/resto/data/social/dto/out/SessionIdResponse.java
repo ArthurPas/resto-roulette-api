ded8cdd973467a89551db84de64c61d8f4ec98f7
arthur <arthur.pascal33@gmail.com>
2026-02-09 13:50:43 +0100

    fix: add return type

---
+++ src/main/java/com/roulette/resto/data/social/dto/out/SessionIdResponse.java
@@ -0,0 +1,12 @@
+package com.roulette.resto.data.social.dto.out;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SessionIdResponse {
+	String sessionId;
+}
